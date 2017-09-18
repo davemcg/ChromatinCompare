fca889142990f74e3b51916c5088ba01e72e3fbc
David McGaughey <mcgaughey@gmail.com>
2017-09-18 09:43:47 -0400

    updated documentation with examples

--- R/bed9_importer.R
+++ R/bed9_importer.R
@@ -1,14 +1,15 @@
-#' Imports bed9 (9 column) files into R 
-#' 
-#' @param A Path to the bed file
-#' @param B If the bed file is compressed, should the uncompressed file be 
+#' Imports bed9 (9 column) files into R
+#'
+#' @param bed Path to the bed file
+#' @param uncompress_keep If the bed file is compressed, should the uncompressed file be
 #' retained or removed? Defaults to TRUE.
-#' @param C How many lines to skip? Defaults to zero. You must look at the 
+#' @param skip How many lines to skip? Defaults to zero. You must look at the
 #' first few lines of the bed file to determine if non bed entries are present.
 #' @details \code{bed9_importer} imports a 9 column bed file and retains proper
-#' encoding (character, integer) of the columns. 
-
-
+#' encoding (character, integer) of the columns.
+#' @example
+#' bed9_importer('~/your_bed_file.bed.gz', skip=1 )
+#' bed9_importer
 bed9_importer <- function(bed, uncompress_keep = TRUE, skip=0){
   not_gzip <- strsplit(bed,'\\.gz')[[1]][1]
   if (file.exists(not_gzip)){
