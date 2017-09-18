adfd664df41cea1459b2eaf56919e7d6c31e94d2
David McGaughey <mcgaughey@gmail.com>
2017-09-18 09:25:31 -0400

    functions and package skeleton commit

---
+++ R/bed9_importer.R
@@ -0,0 +1,24 @@
+#' Imports bed9 (9 column) files into R 
+#' 
+#' @param A Path to the bed file
+#' @param B If the bed file is compressed, should the uncompressed file be 
+#' retained or removed? Defaults to TRUE.
+#' @param C How many lines to skip? Defaults to zero. You must look at the 
+#' first few lines of the bed file to determine if non bed entries are present.
+#' @details \code{bed9_importer} imports a 9 column bed file and retains proper
+#' encoding (character, integer) of the columns. 
+
+
+bed9_importer <- function(bed, uncompress_keep = TRUE, skip=0){
+  not_gzip <- strsplit(bed,'\\.gz')[[1]][1]
+  if (file.exists(not_gzip)){
+    output <- readr::read_tsv(not_gzip, col_names = F, col_types = list('c','i','i','c','i','c','i','i','c'), skip = skip)
+  } else {
+    output <- readr::read_tsv(bed, col_names = F, col_types = list('c','i','i','c','i','c','i','i','c'), skip = skip)
+  }
+  if (!uncompress_keep) {
+    system(paste0('rm ', not_gzip))
+  }
+  colnames(output) <- c('chr','start','end', 'name', 'score', 'strand', 'thickStart', 'thickEnd', 'itemRgb')
+  return(output)
+}
---
+++ R/bed9_to_granges.R
@@ -0,0 +1,13 @@
+#' Converts imported bed file to granges object
+#' 
+#' @param A Imported bed object (from \code{bed9_importer})
+#' @param B Which genome is this? Defaults to mm10. Other options are FILL IN!!
+#' @details \code{bed9_to_granges} Converts an imported bed file to a granges
+#' object for annotation.
+bed9_to_granges <- function(bed_dataframe, genome='mm10'){
+  x <- bed_dataframe
+  bed9_ranges <- GenomicRanges::GRanges(seqnames=x$chr, ranges = IRanges(start=as.numeric(x$start),end=as.numeric(x$end)), name=x$name, score = x$score, strand=(rep('*',length(x$chr))), thickStart = x$thickStart, thickEnd = x$thickEnd, itemRgb = x$itemRgb)
+  GenomeInfoDb::genome(bed9_ranges) <- genome
+  GenomeInfoDb::seqlevels(bed9_ranges) <- sort(GenomeInfoDb::seqlevels(bed9_ranges))
+  return(bed9_ranges)
+}
