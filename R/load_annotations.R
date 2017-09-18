fac470999f947d2277c961a9c99c3d9ec3126d59
David McGaughey <mcgaughey@gmail.com>
2017-09-18 12:23:37 -0400

    added more functions

--- R/load_annotations.R
+++ R/load_annotations.R
@@ -5,26 +5,29 @@
 #' only, intron only.
 #' @details \code{load_annotations} Loads in species and build specific annotations
 #' encoding (character, integer) of the columns.
-#' @example
+#' @examples
 #' annotations <- load_annotations(build= 'mm10', type='gene_byd' )
 #' annotations
 load_annotations <- function(build='mm10', type='gene_body'){
   annots = c('mm10_basicgenes')
   annotations = annotatr::build_annotations(genome = 'mm10', annotations = annots)
-  seqlevelsStyle(annotations) <- "UCSC"
-  GenomeInfodb::seqlevels(annotations) <- sort(GenomeInfodb::seqlevels(annotations))
+  GenomeInfoDb::seqlevelsStyle(annotations) <- "UCSC"
+  GenomeInfoDb::seqlevels(annotations) <- sort(GenomeInfoDb::seqlevels(annotations))
   # remove annotations without a gene symbol and LOC* and LINC*
-  annotations <- annotations %>% data.frame() %>% filter(!is.na(symbol)) %>% filter(!grepl('^LOC', symbol)) %>% filter(!grepl('^LINC',symbol))
+  annotations <- annotations %>% data.frame() %>%
+    dplyr::filter(!is.na(symbol)) %>%
+    dplyr::filter(!grepl('^LOC', symbol)) %>%
+    dplyr::filter(!grepl('^LINC',symbol))
   # intergenic
   annots_intergenic <- c('mm10_genes_intergenic')
   intergenic_annotations = annotatr::build_annotations(genome = 'mm10', annotations = annots_intergenic)
-  GenomeInfodb::seqlevelsStyle(intergenic_annotations) <- "UCSC"
-  GenomeInfodb::seqlevels(intergenic_annotations) <- sort(GenomeInfodb::seqlevels(intergenic_annotations))
+  GenomeInfoDb::seqlevelsStyle(intergenic_annotations) <- "UCSC"
+  GenomeInfoDb::seqlevels(intergenic_annotations) <- sort(GenomeInfoDb::seqlevels(intergenic_annotations))
 
   # merge annotations with intergenic
   annotations <- rbind(annotations, intergenic_annotations %>% data.frame())
   # rebuild granges
   annotations <- GenomicRanges::GRanges(seqnames=annotations$seqnames, ranges=IRanges(start=annotations$start, end = annotations$end), strand = annotations$strand , id = annotations$id, gene_id = annotations$id, symbol = annotations$symbol, type =annotations$type)
-  GenomeInfodb::seqlevels(annotations) <- sort(GenomeInfodb::seqlevels(annotations))
+  GenomeInfoDb::seqlevels(annotations) <- sort(GenomeInfoDb::seqlevels(annotations))
   return(annotations)
 }
